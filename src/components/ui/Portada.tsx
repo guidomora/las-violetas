a42808b7d3c76ae1bb16b82144faed49aa960e61
guido morabito <guidomorabito161@hotmail.com>
2023-09-09 18:06:23 -0300

    Portada

--- src/components/ui/Portada.tsx
+++ src/components/ui/Portada.tsx
@@ -6,7 +6,7 @@ const Portada = () => {
 
   return (
     <Grid>
-      <CardMedia component="img" image='https://firebasestorage.googleapis.com/v0/b/segunda-floreria.appspot.com/o/newPortada.png?alt=media&token=26361baf-5527-4eed-9864-eb86328a2989'
+      <CardMedia component="img" image='https://firebasestorage.googleapis.com/v0/b/segunda-floreria.appspot.com/o/newPortada.png?alt=media&token=cb0f2760-aa84-42da-acd0-296e09701f25'
       sx={{maxHeight:800}}
       className='animate__animated animate__fadeIn'>
       </CardMedia>
